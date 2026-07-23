5e6b85deef0660a8e899fa385291f58bcddc6724
Rafael Espinoza <rafael@rafaelespinoza.com>
2026-07-22 22:13:49 -0700

    update for v0.16.1

--- Formula/godfish.rb
+++ Formula/godfish.rb
@@ -1,27 +1,27 @@
 class Godfish < Formula
-  desc "Database migrations CLIs for cassandra, postgres, mysql, sqlite3, sqlserver"
+  desc "Database migrations CLI for cassandra, postgres, mysql, sqlite3, sqlserver"
   homepage "https://github.com/rafaelespinoza/godfish"
-  version "0.16.0"
+  version "0.16.1"
   license "ISC"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/rafaelespinoza/godfish/releases/download/v0.16.0/godfish_0.16.0_darwin_amd64.tar.gz"
-    sha256 "7ae0a5711b402d505763209406b20d9336631a04d238a2503b45b7f77e9b5cb7"
+    url "https://github.com/rafaelespinoza/godfish/releases/download/v0.16.1/godfish_0.16.1_darwin_amd64.tar.gz"
+    sha256 "4be4efc2a1e2be6c3fb7ad6a098c9f67595a94213134870d32c78e15579d8cb3"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/rafaelespinoza/godfish/releases/download/v0.16.0/godfish_0.16.0_darwin_arm64.tar.gz"
-    sha256 "cfacc2dece17883f416853cf54051a20e86e3513741f87d1d5a9f1b5f4864c3e"
+    url "https://github.com/rafaelespinoza/godfish/releases/download/v0.16.1/godfish_0.16.1_darwin_arm64.tar.gz"
+    sha256 "961ae77c0f40b45b7ab39fb8e232f8b4e1ef2d3c3d14a250b53b0a7661d374e9"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/rafaelespinoza/godfish/releases/download/v0.16.0/godfish_0.16.0_linux_amd64.tar.gz"
-    sha256 "487f1457965bbc2586dc601bb6f020237255f88a35787cf2d985a48961e5e156"
+    url "https://github.com/rafaelespinoza/godfish/releases/download/v0.16.1/godfish_0.16.1_linux_amd64.tar.gz"
+    sha256 "d0526260cc037044140e07cc6864f13b32ad9d174c2973038ff80adcef35d1db"
   end
 
   if OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/rafaelespinoza/godfish/releases/download/v0.16.0/godfish_0.16.0_linux_arm64.tar.gz"
-    sha256 "64b930097eecc8930f6ba0c34adc1b48061cb993cb7d6a64155a102ac1d553eb"
+    url "https://github.com/rafaelespinoza/godfish/releases/download/v0.16.1/godfish_0.16.1_linux_arm64.tar.gz"
+    sha256 "ff3de7d70d53cc4f0387f013c147df2001571fc1a2a0def4a7bd61dda2af4a0c"
   end
 
   def install
